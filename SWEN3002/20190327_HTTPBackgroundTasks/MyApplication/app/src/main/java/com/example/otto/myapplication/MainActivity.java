7a5cc76831b9ced06d9af7a49adb5a33bd7a9039
otboss <otsurfer6@gmail.com>
2019-05-11 21:36:32 -0500

    Minor Changes

--- SWEN3002/20190327_HTTPBackgroundTasks/MyApplication/app/src/main/java/com/example/otto/myapplication/MainActivity.java
+++ SWEN3002/20190327_HTTPBackgroundTasks/MyApplication/app/src/main/java/com/example/otto/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.otto.myapplication;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -13,7 +15,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import java.util.concurrent.Future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,8 +50,25 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    @TargetApi(Build.VERSION_CODES.N)
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private CompletableFuture<StringRequest> sendRequest(String url){
+        CompletableFuture<StringRequest> response = new CompletableFuture<>();
 
-
+        Executors.newCachedThreadPool().submit(()-> {
+            response.complete(new StringRequest(Request.Method.GET, url, response1 -> {
+                // Display the first 500 characters of the response string.
+                Log.i("myApp","Response is: "+ response1.substring(0,500));
+                res = gson.fromJson(response1, FlaskServerResponse.class);
+                res.isNull = false;
+            }, error -> {
+                Log.i("myApp","That didn't work!");
+                throw new Error("Error Here");
+            }));
+            return null;
+        });
+        return response;
+    }
 
 
     @Override
@@ -57,11 +78,12 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
+        //TODO: Implement using CompletableFuture<V>. The following implementation will hand the main thread!
         sendJsonRequest("https://jsonplaceholder.typicode.com/todos/1");
         while(res.isNull){
             //WAITING ON SERVER RESPONSE..
         }
+        //sendRequest("https://jsonplaceholder.typicode.com/todos/1").complete();
         res.isNull = true;
 
         //Load the results onto the screen
